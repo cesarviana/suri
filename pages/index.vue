b6df506d409709c84f295ee66cb4aed7ab3ad762
Cesar Viana <cesar.pviana@gmail.com>
2019-11-26 13:05:07 -0300

    Creating vehycles

--- pages/index.vue
+++ pages/index.vue
@@ -1,39 +1,78 @@
 <template>
   <div class="container">
     <div>
-      <logo />
-      <h1 class="title">
-        suri_simulation
-      </h1>
-      <h2 class="subtitle">
-        Agent-based simulation of in emergency vehycles behaviour 
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      <h1>SURI - Ambiente de simulação</h1>
+
+      <canvas id="myCanvas" ref="canvas" :width="canvas.width" :height="canvas.height" style="border:1px solid #000000;"></canvas>
+
     </div>
   </div>
 </template>
 
 <script>
 import Logo from '~/components/Logo.vue'
+import jssim from 'js-simulator'
+
+class Vehycle extends jssim.SimEvent {
+  constructor(id, x, y, space){
+    super()
+    const rank = 1
+    jssim.SimEvent.call(this, rank)
+    this.id = id
+    this.space = space
+    this.space.updateAgent(this, x, y)
+    this.velocity = new jssim.Vector2D(0.5, 0.5)
+
+    this.border = 30
+    this.color = 'red'
+    this.size = new jssim.Vector2D(10, 10)
+  }
+  update() {
+    const pos = this.space.getLocation(this.id)
+    pos.x = 1
+    pos.y = 150
+  }
+}
 
 export default {
   components: {
     Logo
+  },
+  data() {
+    return {
+      maxVehycles: 10,
+      canvas: {
+        width: 640,
+        height: 640
+      }
+    }
+  },
+  mounted() {
+    this.startSimulation()
+  },
+  methods: {
+    startSimulation() {
+      
+      const scheduler = new jssim.Scheduler()
+      scheduler.reset()
+
+      const space = new jssim.Space2D()
+      space.reset()
+
+      for(let i=0; i<this.maxVehycles; i++){
+        const x = Math.ceil(Math.random() * this.canvas.width)
+        const y = Math.ceil(Math.random() * this.canvas.height)
+        console.log(x, y)
+        const vehycle = new Vehycle(i, x, y, space)
+        scheduler.scheduleRepeatingIn(vehycle, 1)
+      }
+
+      const canvas = this.$refs.canvas
+      setInterval(()=>{
+        scheduler.update()
+        space.render(canvas)
+      }, 100)
+    }
   }
 }
 </script>
